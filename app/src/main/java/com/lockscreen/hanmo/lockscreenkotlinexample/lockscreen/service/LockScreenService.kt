64bd2d99d21222ca3d7f4473a2512a38b924c9a7
hanmolee <dlgksah@gmailc.om>
2018-10-02 15:10:08 +0900

    잠금화면 서비스를 관리하면 Object를 구현 #2

--- app/src/main/java/com/lockscreen/hanmo/lockscreenkotlinexample/lockscreen/service/LockScreenService.kt
+++ app/src/main/java/com/lockscreen/hanmo/lockscreenkotlinexample/lockscreen/service/LockScreenService.kt
@@ -10,6 +10,8 @@ import android.os.IBinder
  */
 
 class LockScreenService : Service(){
+
+
     override fun onBind(intent: Intent?): IBinder? {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
--- app/src/main/java/com/lockscreen/hanmo/lockscreenkotlinexample/lockscreen/util/LockScreen.kt
+++ app/src/main/java/com/lockscreen/hanmo/lockscreenkotlinexample/lockscreen/util/LockScreen.kt
@@ -1,5 +1,12 @@
 package com.lockscreen.hanmo.lockscreenkotlinexample.lockscreen.util
 
+import android.app.ActivityManager
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import com.lockscreen.hanmo.lockscreenkotlinexample.LockScreenApplication
+import com.lockscreen.hanmo.lockscreenkotlinexample.lockscreen.service.LockScreenService
+
 /**
  * 잠금화면 Service를 관리하는 Class
  * Created by hanmo on 2018. 10. 2..
@@ -8,11 +15,37 @@ package com.lockscreen.hanmo.lockscreenkotlinexample.lockscreen.util
 object LockScreen {
 
     fun active() {
-
+        LockScreenApplication.applicationContext()?.run {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                startForegroundService(Intent(this, LockScreenService::class.java))
+            } else {
+                startService(Intent(this, LockScreenService::class.java))
+            }
+        }
     }
 
     fun deActivate() {
+        LockScreenApplication.applicationContext()?.run {
+            stopService(Intent(this, LockScreenService::class.java))
+        }
+    }
 
+
+    val isActive: Boolean
+        get() = LockScreenApplication.applicationContext()?.let {
+            isMyServiceRunning(LockScreenService::class.java)
+        } ?: kotlin.run {
+            false
+        }
+
+    private fun isMyServiceRunning(serviceClass: Class<*>): Boolean {
+        val manager = LockScreenApplication.applicationContext()?.getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager
+        for (service in manager.getRunningServices(Integer.MAX_VALUE)) {
+            if (serviceClass.name == service.service.className) {
+                return true
+            }
+        }
+        return false
     }
 
 }
