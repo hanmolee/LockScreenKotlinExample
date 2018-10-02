9949bdf4087ef4c8707b34073d7c3ead1d49b0e9
hanmolee <dlgksah@gmailc.om>
2018-10-02 16:20:05 +0900

    잠금화면 Activity를 구현 및 화면 스와이프 시 finish 구현 #4

--- app/src/main/java/com/lockscreen/hanmo/lockscreenkotlinexample/lockscreen/LockScreenActivity.kt
+++ app/src/main/java/com/lockscreen/hanmo/lockscreenkotlinexample/lockscreen/LockScreenActivity.kt
@@ -1,6 +1,16 @@
 package com.lockscreen.hanmo.lockscreenkotlinexample.lockscreen
 
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.view.WindowManager
+import com.lockscreen.hanmo.lockscreenkotlinexample.R
+import com.lockscreen.hanmo.lockscreenkotlinexample.lockscreen.util.UnLock
+import io.reactivex.disposables.CompositeDisposable
+import kotlinx.android.synthetic.main.activty_lockscreen.*
 
 /**
  * 잠금화면 Activity
@@ -9,5 +19,61 @@ import android.support.v7.app.AppCompatActivity
 
 class LockScreenActivity : AppCompatActivity() {
 
+    private val compositeDisposable: CompositeDisposable by lazy { CompositeDisposable() }
 
+    companion object {
+        fun newIntent(context: Context?) : Intent {
+            return Intent(context, LockScreenActivity::class.java)
+                    .apply {
+                        addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                        addFlags(Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS)
+                    }
+        }
+    }
+
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        super.onWindowFocusChanged(hasFocus)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            if (hasFocus) window.decorView.systemUiVisibility = (
+                    View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                            or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    )
+        }
+    }
+
+    @Suppress("DEPRECATION")
+    override fun onAttachedToWindow() {
+        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON
+                or WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD
+                or WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED
+        )
+
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+        super.onAttachedToWindow()
+    }
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activty_lockscreen)
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        setUnlock()
+    }
+
+    private fun setUnlock() {
+        lockScreenView.x = 0f
+
+        lockScreenView.setOnTouchListener(object : UnLock(this, lockScreenView) {
+            override fun onFinish() {
+                finish()
+                super.onFinish()
+            }
+        })
+    }
 }
---
+++ app/src/main/java/com/lockscreen/hanmo/lockscreenkotlinexample/lockscreen/util/UnLock.kt
@@ -0,0 +1,105 @@
+package com.lockscreen.hanmo.lockscreenkotlinexample.lockscreen.util
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.support.constraint.ConstraintLayout
+import android.view.MotionEvent
+import android.view.View
+import android.view.animation.Animation
+import android.view.animation.TranslateAnimation
+
+/**
+ * 잠금화면을 스와이프하면 finish 하도록 하는 touch util
+ * Created by hanmo on 2018. 10. 2..
+ */
+
+open class UnLock(val context: Context, val lockScreenView: ConstraintLayout) : View.OnTouchListener {
+
+    private var firstTouchX = 0f
+    private var layoutPrevX = 0f
+    private var lastLayoutX = 0f
+    private var layoutInPrevX = 0f
+    private var isLockOpen = false
+    private var touchMoveX = 0
+
+    @SuppressLint("ClickableViewAccessibility")
+    override fun onTouch(v: View, event: MotionEvent): Boolean {
+        when (event.action and MotionEvent.ACTION_MASK) {
+            MotionEvent.ACTION_DOWN -> {
+                firstTouchX = event.x + 5f
+                layoutPrevX = lockScreenView.x
+                isLockOpen = true
+
+            }
+
+            MotionEvent.ACTION_MOVE -> {
+                if (isLockOpen) {
+                    touchMoveX = (event.rawX - firstTouchX).toInt()
+                    if (lockScreenView.x >= 0) {
+                        lockScreenView.x = (layoutPrevX + touchMoveX).toInt().toFloat()
+                        if (lockScreenView.x < 0) {
+                            lockScreenView.x = 0f
+                        }
+
+                        lastLayoutX = lockScreenView.x
+                    }
+                } else {
+                    return false
+                }
+            }
+            MotionEvent.ACTION_UP -> { // 1
+                if (isLockOpen) {
+                    lockScreenView.x = lastLayoutX
+                    lockScreenView.y = 0f
+                    optimizeForground(lastLayoutX)
+                }
+                isLockOpen = false
+                firstTouchX = 0f
+                layoutPrevX = 0f
+                layoutInPrevX = 0f
+                touchMoveX = 0
+                lastLayoutX = 0f
+            }
+            else -> {
+            }
+        }
+
+        return true
+    }
+
+
+    private fun optimizeForground(forgroundX: Float) {
+
+        val displayMetrics = context.resources.displayMetrics
+        val mDeviceWidth = displayMetrics.widthPixels
+        val mDevideDeviceWidth = mDeviceWidth / 6
+
+        if (forgroundX < mDevideDeviceWidth) {
+            var startPosition = mDevideDeviceWidth
+            while (startPosition >= 0) {
+                lockScreenView.x = startPosition.toFloat()
+                startPosition--
+            }
+        } else {
+            val animation = TranslateAnimation(0f, mDeviceWidth.toFloat(), 0f, 0f)
+            animation.duration = 300
+            animation.setAnimationListener(object : Animation.AnimationListener {
+                override fun onAnimationStart(animation: Animation) {}
+
+                override fun onAnimationEnd(animation: Animation) {
+                    lockScreenView.x = mDeviceWidth.toFloat()
+                    lockScreenView.y = 0f
+                    onFinish()
+                }
+
+                override fun onAnimationRepeat(animation: Animation) {}
+            })
+
+            lockScreenView.startAnimation(animation)
+        }
+    }
+
+    open fun onFinish() {
+
+    }
+}
